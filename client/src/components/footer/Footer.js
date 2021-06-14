38f2d7b1e246cae33f4fc3a1658eaf8ce2eea56a
rizwann <rizwankabirsizan@gmail.com>
2021-06-14 17:36:20 +0200

    ADD: Finalized BackEnd

---
+++ client/src/components/footer/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <div>
+            <center><footer>© 2021 Copyright: Rizwan Kabir</footer></center>
+        </div>
+    )
+}
---
+++ client/src/components/mainpages/detailProduct/DetailProduct.js
@@ -0,0 +1,71 @@
+import React, {useContext, useState, useEffect} from 'react'
+import {useParams, Link} from 'react-router-dom'
+import {GlobalState} from '../../../GlobalState'
+import ProductItem from '../utils/productItem/ProductItem'
+
+
+function DetailProduct() {
+    const params = useParams()
+    const state = useContext(GlobalState)
+    const [products] = state.productsAPI.products
+    const addCart = state.userAPI.addCart
+    const [detailProduct, setDetailProduct] = useState([])
+
+    useEffect(() =>{
+        if(params.id){
+
+            products.forEach(product => {
+                if(product._id === params.id) setDetailProduct(product)
+            })
+        }
+    },[params.id, products])
+
+    if(detailProduct.length === 0) return null;
+const defl= "https://www.youtube.com/embed/uXl77UFkrkQ"
+    return (
+        <>
+            <div className="detail">
+                {/* <img src={detailProduct.images.url} alt="" /> */}
+                <div >
+    <iframe
+      width="853"
+      height="480"
+      src={detailProduct.trailer? detailProduct.trailer:defl }
+      frameBorder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowFullScreen
+      title="Embedded youtube"
+    />
+  </div>
+                <div className="box-detail">
+                    <div className="row">
+                        <h2>{detailProduct.title}</h2>
+                        <h6>#id: {detailProduct.product_id}</h6>
+                    </div>
+                    <span>€ {detailProduct.price}</span>
+                    <p>{detailProduct.description}</p>
+                    <p>{detailProduct.content}</p>
+                    <p>Rent: {detailProduct.sold}</p>
+                    <Link to="/cart" className="cart"
+                    onClick={() => addCart(detailProduct)}>
+                        Rent Now
+                    </Link>
+                </div>
+            </div>
+
+            <div>
+                <h2>Related products</h2>
+                <div className="products">
+                    {
+                        products.map(product => {
+                            return product.category === detailProduct.category 
+                                ? <ProductItem key={product._id} product={product} /> : null
+                        })
+                    }
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default DetailProduct
